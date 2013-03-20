a3489ba3304a85c036e0de8d1381b1f6e9e68d74
Michael <mnichau@yahoo.de>
2013-03-20 00:58:46 +0000

    Update tests to the new hash format

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1', :text => 'Sample App') }
+    it { should have_selector('h1', text: 'Sample App') }
     it { should have_title(base_title) }
     it { should_not have_title('| Home') }
     it { should_not have_title('|') }
@@ -18,21 +18,21 @@ describe "Static pages" do
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_title("#{base_title} | Help") }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_selector('h1', text: 'About Us') }
     it { should have_title("#{base_title} | About Us") }
   end
 
   describe "Contact" do
     before { visit contact_path }
 
-    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title("#{base_title} | Contact") }
   end
 end
