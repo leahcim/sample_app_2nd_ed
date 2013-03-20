c20b1ba34576860bfe2dcec0fcc54487634cb31b
Michael <mnichau@yahoo.de>
2013-03-20 00:37:32 +0000

    Remove page visit duplication in tests

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,63 +5,57 @@ describe "Static pages" do
   let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
   describe "Home page" do
+    before { visit root_path }
 
     it "should have the h1 'Sample App'" do
-      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the base title" do
-      visit root_path
       page.should have_title(base_title)
     end
 
     it "should not have a custom page title" do
-      visit root_path
       page.should_not have_title('| Home')
     end
 
     it "should not have a title divider '|'" do
-      visit root_path
       page.should_not have_title('|')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
     it "should have the h1 'Help'" do
-      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
       page.should have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
 
     it "should have the h1 'About Us'" do
-      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit about_path
       page.should have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact" do
+    before { visit contact_path }
 
     it "should have the h1 'Contact'" do
-      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       page.should have_title("#{base_title} | Contact")
     end
   end
