da67927df9c802adc554eb6784783dc5bb019cd0
Michael <mnichau@yahoo.de>
2013-03-30 22:18:28 +0000

    Add User authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 
@@ -19,4 +20,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
